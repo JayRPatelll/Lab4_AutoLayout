5602305e7561a0485bc5466097423bd1a8ffe03c
JayRPatelll <145155617+JayRPatelll@users.noreply.github.com>
2023-10-06 19:31:07 -0400

    Add files via upload
    Add Lab4 AutoLayout Assignment.

---
+++ Lab4_JayPatel_AutoLayout/Lab4_JayPatel_AutoLayout/Lab4_JayPatel_AutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab4_JayPatel_AutoLayout
+//
+//  Created by user239463 on 10/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
